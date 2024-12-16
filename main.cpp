91979ef5bd327c08e6f87f29b7373c1244931d8e
BartlomiejZywicki <255745@edu.p.lodz.pl>
2024-12-16 08:53:23 +0100

    oe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+int Policz(int a, int b) {
+    return a + b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << Policz(1, 2) << std::endl;
     return 0;
 }
